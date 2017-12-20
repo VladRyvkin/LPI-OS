3caea2063c87623c2625cb749508d285f76bd624
VladRyvkin <34416791+VladRyvkin@users.noreply.github.com>
2017-12-20 14:42:15 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -16,7 +16,11 @@ while true; do
     read -p "Do you wish to continue?" yn
     case $yn in
         [Yy]* ) sleep 1s; echo ""; echo "Starting installing..."; echo "";  chmod +x fullinst.sh; sh fullinst.sh; sleep 1s; echo "Install complete"; echo ""; sleep 1s; break;;
-        [Nn]* ) sleep 1s; echo ""; echo "Exit..."; echo ""; sleep 1s; echo "Install complete"; echo ""; sleep 1s; exit;;
+        [Nn]* ) sleep 1s; echo ""; echo "Exit..."; echo ""; sleep 1s; echo "Install complete"; echo ""; 
+        echo "https://software.opensuse.org/package/xfce4-panel-plugin-whiskermenu  - new app menu"; echo "";
+        echo "https://software.opensuse.org/package/lightdm-gtk-greeter-settings  - login theme settings"; echo "";
+        echo "https://software.opensuse.org/package/mugshot?search_term=mugshot - login theme settings"; echo "";
+        sleep 1s; exit;;
         * ) echo "Please answer yes or no.(y or n)";;
     esac
 done
