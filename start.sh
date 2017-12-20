b3563a3afb90b1373723a4915c692ef9a836dc52
VladRyvkin <34416791+VladRyvkin@users.noreply.github.com>
2017-12-20 21:34:24 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -15,7 +15,10 @@ sleep 1s
 while true; do
     read -p "Do you wish to continue?" yn
     case $yn in
-        [Yy]* ) sleep 1s; echo ""; echo "Starting installing..."; echo "";  chmod +x fullinst.sh; sh fullinst.sh; sleep 1s; echo "Install complete"; echo ""; sleep 1s; break;;
+        [Yy]* ) sleep 1s; echo ""; echo "Starting installing..."; echo "";  chmod +x fullinst.sh; sh fullinst.sh; sleep 1s; echo "Install complete"; echo "";
+        echo "https://software.opensuse.org/package/lightdm-gtk-greeter-settings  - login theme settings"; echo "";
+        echo "https://software.opensuse.org/package/mugshot?search_term=mugshot - login theme settings"; echo "";
+        sleep 1s; break;;
         [Nn]* ) sleep 1s; echo ""; echo "Exit..."; echo ""; sleep 1s; echo "Install complete"; echo "";
         echo "https://software.opensuse.org/package/lightdm-gtk-greeter-settings  - login theme settings"; echo "";
         echo "https://software.opensuse.org/package/mugshot?search_term=mugshot - login theme settings"; echo "";
