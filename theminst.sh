1582dae15dad94d866eaa68f6ad509fe13c0e91c
VladRyvkin <34416791+VladRyvkin@users.noreply.github.com>
2017-12-20 21:16:58 +0200

    Update theminst.sh

--- theminst.sh
+++ theminst.sh
@@ -28,8 +28,7 @@ while true; do
 		zypper install xfce4-panel-plugin-systemload;
 		zypper install xfce4-panel-plugin-diskperf;
 		zypper install xfce4-panel-plugin-sensors;
-		#xfce4-panel-plugin-whiskermenu;
-		#xfce4-panel-plugin-weather;
+		zypper install xfce4-panel-plugin-weather;
 		unzip -t *.zip;
 		unzip themes.zip -d /usr/share/themes/;
 		unzip icons1.zip -d /usr/share/icons/;
