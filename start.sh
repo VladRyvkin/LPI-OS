50e4df6dc57ebe76b48c41daae669b4f10039d16
VladRyvkin <34416791+VladRyvkin@users.noreply.github.com>
2017-12-20 21:15:05 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -16,8 +16,7 @@ while true; do
     read -p "Do you wish to continue?" yn
     case $yn in
         [Yy]* ) sleep 1s; echo ""; echo "Starting installing..."; echo "";  chmod +x fullinst.sh; sh fullinst.sh; sleep 1s; echo "Install complete"; echo ""; sleep 1s; break;;
-        [Nn]* ) sleep 1s; echo ""; echo "Exit..."; echo ""; sleep 1s; echo "Install complete"; echo ""; 
-        echo "https://software.opensuse.org/package/xfce4-panel-plugin-whiskermenu  - new app menu"; echo "";
+        [Nn]* ) sleep 1s; echo ""; echo "Exit..."; echo ""; sleep 1s; echo "Install complete"; echo "";
         echo "https://software.opensuse.org/package/lightdm-gtk-greeter-settings  - login theme settings"; echo "";
         echo "https://software.opensuse.org/package/mugshot?search_term=mugshot - login theme settings"; echo "";
         echo "https://software.opensuse.org/download.html?project=X11%3Axfce&package=xfce4-panel-plugin-weather"; echo "";
