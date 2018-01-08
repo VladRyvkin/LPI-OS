3afb786f272d7a294e8584513d937957da890f81
VladRyvkin <34416791+VladRyvkin@users.noreply.github.com>
2018-01-08 21:35:34 +0200

    Add files via upload

--- theminst.sh
+++ theminst.sh
@@ -16,10 +16,74 @@ while true; do
     case $yn in
         [Yy]* ) sleep 1s; echo ""; echo "Starting theminst.sh";
 		sleep 1s; echo ""; echo "Copying themes";
-		unzip -t *.zip
-		unzip themes.zip -d /usr/share/themes/;
-		unzip icons.zip -d /usr/share/icons/;
-		rm *.zip;
+		zypper install cinnamon-metatheme-numix;
+		zypper install xfce4-panel-plugin-eyes;
+		zypper install xfce4-panel-plugin-cpufreq;
+		zypper install xfce4-panel-plugin-netload;
+		zypper install xfce4-panel-plugin-fsguard;
+		zypper install xfce4-panel-plugin-wavelan;
+		zypper install xfce4-panel-plugin-cpugraph;
+		zypper install xfce4-panel-plugin-systemload;
+		zypper install xfce4-panel-plugin-diskperf;
+		zypper install xfce4-panel-plugin-sensors;
+		zypper install xfce4-panel-plugin-weather;
+		zypper install xfce4-panel-plugin-whiskermenu;
+		
+		wget https://codeload.github.com/VladRyvkin/ThemesRes-LPI/zip/master;
+		unzip master;
+
+		a=$PWD;
+		cd $PWD/ThemesRes-LPI-master;
+		cp -rp /$PWD/backgrounds/* /usr/share/wallpapers/xfce/
+		
+		cd icons;
+		echo $PWD;
+		unzip Adwaita.zip -d /usr/share/icons/;
+		unzip Breeze.zip -d /usr/share/icons/;
+		unzip bridge.zip -d /usr/share/icons/;
+		unzip capitaine-cursors.zip -d /usr/share/icons/;
+		unzip elementary-xfce.zip -d /usr/share/icons/;
+		unzip elementary.zip -d /usr/share/icons/;
+		unzip gnome.zip -d /usr/share/icons/;
+		unzip Faenza-p1.zip -d /usr/share/icons/;
+		unzip Faenza-p2.zip -d /usr/share/icons/;
+		unzip Faenza-p3.zip -d /usr/share/icons/;
+		unzip Faenza-p4.zip -d /usr/share/icons/;
+		unzip Flat Remix.zip -d /usr/share/icons/;
+		unzip Humanity.zip -d /usr/share/icons/;
+		unzip Numix.zip -d /usr/share/icons/;
+		unzip plane-dark.zip -d /usr/share/icons/;
+		unzip PolarCursorTheme.zip -d /usr/share/icons/;
+		unzip Vertex-Maia-p1.zip -d /usr/share/icons/;
+		unzip Vertex-Maia-p2.zip -d /usr/share/icons/;
+		unzip Vibrancy-Colors-p1.zip -d /usr/share/icons/;
+		unzip Vibrancy-Colors-p2.zip -d /usr/share/icons/;
+		unzip Vibrancy-Colors-p3.zip -d /usr/share/icons/;
+		unzip Vertex-Maia-p1.zip -d /usr/share/icons/;
+		unzip Vertex-Maia-p2.zip -d /usr/share/icons/;
+		unzip win8.zip -d /usr/share/icons/;
+		cp cab_extract.png /usr/share/icons/;
+		cp cab_view.png /usr/share/icons/;
+		cp whiskermenu-manjaro.svg /usr/share/icons/;
+
+		cd $OLDPWD
+
+		cd themes
+		sleep 5s;
+		unzip Adwaita.zip -d /usr/share/themes/;
+		unzip axiom.zip -d /usr/share/themes/;
+		unzip Breath.zip -d /usr/share/themes/;
+		unzip Daloa.zip -d /usr/share/themes/;
+		unzip Greybird.zip -d /usr/share/themes/;
+		#unzip Numix.zip -d /usr/share/themes/;
+		unzip Raleigh.zip -d /usr/share/themes/;
+		unzip RedmondXP.zip -d /usr/share/themes/;
+		unzip Vertex-Maia.zip -d /usr/share/themes/;
+
+		cd $a;
+		rm master;
+		rm -rf ThemesRes-LPI-master;
+		
 		sleep 1s; echo ""; echo "Copying themes complete"; echo ""; break;;
         [Nn]* ) sleep 1s; echo ""; echo "Close theminst.sh"; sleep 1s; echo ""; exit;;
         * ) echo "Please answer yes or no.(y or n)";;
