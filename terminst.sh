c7bae3ce9440c37ec3d32f97ad000e1d1897b957
VladRyvkin <34416791+VladRyvkin@users.noreply.github.com>
2018-01-08 17:10:27 +0200

    Update terminst.sh

--- terminst.sh
+++ terminst.sh
@@ -157,6 +157,16 @@ while true; do
 				* ) echo "Please answer yes or no.(y or n)";;
 			 	esac
 			done
+		while true; do
+	   	 	read -p "Do you wish to install gnome-system-monitor?" yn
+	   	 	case $yn in
+       		 		[Yy]* ) sleep 1s; echo ""; echo "Installing gnome-system-monitor...";
+					zypper install zypper install gnome-system-monitor;
+					sleep 1s; echo ""; echo "Installing gnome-system-monitor complete"; echo ""; break;;
+				[Nn]* ) sleep 1s; echo ""; break;;
+				* ) echo "Please answer yes or no.(y or n)";;
+			 	esac
+done
 		
 
 		sleep 1s; echo ""; echo "Close terminst.sh"; echo ""; break;;
