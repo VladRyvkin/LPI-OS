416c7717101d20e214a33bd312e239195b912a9a
Vladyslav Ryvkin <vladryvkin96@gmail.com>
2023-05-16 22:03:21 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
 
+echo ""
 echo "Operating System: $(lsb_release -s -d)"
-sleep 1s
+sleep 0.3s
 echo "start from user: $USER"
-sleep 1s
+sleep 0.3s
 echo ""
 echo "working directory : $PWD"
-sleep 1s
+sleep 0.3s
 echo ""
-sleep 1s
+sleep 0.3s
 echo "Welcome to the LPI-OS"
 echo "linux post installer for $(lsb_release -s -d)"
 echo ""
-sleep 1s
+sleep 0.3s
 
 while true; do
     read -p "Do you wish to continue?" yn
     case $yn in
-        [Yy]* ) sleep 1s; echo ""; echo "Starting installing..."; echo "";  chmod +x fullinst.sh; sh fullinst.sh; sleep 1s; echo "Install complete"; echo "";
+        [Yy]* ) sleep 0.3s; echo ""; echo "Starting installing..."; echo "";  chmod +x fullinst.sh; sh fullinst.sh; sleep 0.3s; echo "Install complete"; echo "";
         echo "https://software.opensuse.org/package/lightdm-gtk-greeter-settings  - login theme settings"; echo "";
         echo "https://software.opensuse.org/package/mugshot?search_term=mugshot - login theme settings"; echo "";
-        sleep 1s; break;;
-        [Nn]* ) sleep 1s; echo ""; echo "Exit..."; echo ""; sleep 1s; echo "Install complete"; echo "";
+        sleep 0.3s; break;;
+        [Nn]* ) sleep 0.3s; echo ""; echo "Exit..."; echo ""; sleep 0.3s; echo "Install complete"; echo "";
         echo "https://software.opensuse.org/package/lightdm-gtk-greeter-settings  - login theme settings"; echo "";
         echo "https://software.opensuse.org/package/mugshot?search_term=mugshot - login theme settings"; echo "";
-        sleep 1s; exit;;
+        sleep 0.3s; exit;;
         * ) echo "Please answer yes or no.(y or n)";;
     esac
 done
