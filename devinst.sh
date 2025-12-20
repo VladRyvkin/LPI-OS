277ea75e15ec979fdff0aa259325889b4ce7ed14
Vladyslav Ryvkin <vladryvkin96@gmail.com>
2025-12-20 10:48:17 -0600

    Update devinst.sh

--- devinst.sh
+++ devinst.sh
@@ -9,7 +9,7 @@ sleep 0.3s;
 echo "In this file we have IDE:"
 sleep 0.3s;
 echo ""
-echo "Atom, Eclipse, Android Studio"
+echo "Atom, Android Studio"
 sleep 0.3s;
 echo ""
 sleep 0.3s;
@@ -30,31 +30,29 @@ while true; do
 				* ) echo "Please answer yes or no.(y or n)";;
 			 	esac
 			done
-		while true; do
-	   	 	read -p "Do you wish to install Eclipse?" yn
-	   	 	case $yn in
-       		 		[Yy]* ) sleep 0.3s; echo ""; echo "Installing eclipse";
-					wget http://mirror.tspu.ru/eclipse/oomph/epp/oxygen/R/eclipse-inst-linux64.tar.gz;
-					tar -xf eclipse-inst-linux64.tar.gz;
-					cd /$PWD/eclipse-installer/
-					./eclipse-inst;
-					cd $OLDPWD;
-					rm eclipse-inst-linux64.tar.gz;
-					rm -rf /$OLDPWD/eclipse-installer;
-					sleep 0.3s; echo ""; echo "Installing eclipse complete"; echo ""; break;;
-				[Nn]* ) sleep 0.3s; echo ""; break;;
-				* ) echo "Please answer yes or no.(y or n)";;
-			 	esac
-			done
+		#while true; do
+	   	 	#read -p "Do you wish to install Eclipse?" yn
+	   	 	#case $yn in
+       		 		#[Yy]* ) sleep 0.3s; echo ""; echo "Installing eclipse";
+					#sudo zypper addrepo --refresh https://download.opensuse.org/repositories/system:/snappy/openSUSE_Tumbleweed snappy;
+					#sudo zypper --gpg-auto-import-keys refresh;
+					#sudo zypper dup --from snappy;
+					#sudo zypper install snapd;
+					#sudo systemctl enable --now snapd;
+					#sudo systemctl enable --now snapd.apparmor;
+					#sudo snap install eclipse --classic;
+					#sleep 0.3s; echo ""; echo "Installing eclipse complete"; echo ""; break;;
+				#[Nn]* ) sleep 0.3s; echo ""; break;;
+				#* ) echo "Please answer yes or no.(y or n)";;
+			 	#esac
+			#done
 		while true; do
 	   	 	read -p "Do you wish to install Android Studio?" yn
 	   	 	case $yn in
        		 		[Yy]* ) sleep 0.3s; echo ""; echo "Installing Android Studio";
-					wget https://dl.google.com/dl/android/studio/ide-zips/3.0.1.0/android-studio-ide-171.4443003-linux.zip;
-					unzip -t *.zip;
-					unzip *.zip -d /opt/;
-					rm *.zip;
-					sh /opt/android-studio/bin/studio.sh;
+					sudo zypper addrepo https://download.opensuse.org/repositories/home:ecsos/openSUSE_Tumbleweed/home:ecsos.repo;
+					sudo zypper refresh --gpg-auto-import-keys;
+					sudo zypper install android-studio;
 					sleep 0.3s; echo ""; echo "Installing Android Studio complete"; echo ""; break;;
 				[Nn]* ) sleep 0.3s; echo ""; break;;
 				* ) echo "Please answer yes or no.(y or n)";;
@@ -65,8 +63,3 @@ while true; do
         * ) echo "Please answer yes or no.(y or n)";;
     esac
 done
-
-#unzip -t /home/vlad/Downloads/open_suse02/bin/android-studio-ide-171.4443003-linux.zip
-#unzip /home/vlad/Downloads/open_suse02/bin/android-studio-ide-171.4443003-linux.zip -d /opt/
-##sudo zypper install zlib.i686 ncurses-libs.i686 bzip2-libs.i686
-#sh /opt/android-studio/bin/studio.sh
